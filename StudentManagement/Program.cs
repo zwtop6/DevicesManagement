acc3b478c166e619656e051f9aa63190932da77f
zwtop6 <48090671+zwtop6@users.noreply.github.com>
2020-11-13 19:55:12 +0800

    该名称student为Device

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace StudentManagement
+namespace DeviceManagement
 {
     public class Program
     {
