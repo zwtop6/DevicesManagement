63c25f4b8c0c9d61afc5c5a29f4a3348bb709049
zwtop6 <897289382@qq.com>
2020-12-04 22:16:28 +0800

    test

--- StudentManagement/Startup.cs
+++ StudentManagement/Startup.cs
@@ -28,6 +28,7 @@ namespace DeviceManagement
             _configuration = configuration;
         }
 
+        //test
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
